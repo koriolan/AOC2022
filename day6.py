c7321204d2c1815cdb4f8c7db1c6c52aff08e2fb
Anton <a.bragin@saga-tech.ru>
2022-12-06 10:01:03 +0300

    решение 6 задачи

--- day6.py
+++ day6.py
@@ -1,22 +1,19 @@
 with open('input/6.txt') as f:
     key = f.read(4)
-    i = 4
     for l in f.read():
-        if len(set(key)) == 4:
+        if len(set(key[-4:])) == 4:
             break
         else:
-            key = key[1:] + l
-            i += 1
+            key += l
+print(f'part 1: {len(key)}')
 
 with open('input/6.txt') as f:
         key = f.read(14)
-        j = 14
         for l in f.read():
-            if len(set(key)) == 14:
+            if len(set(key[-14:])) == 14:
                 break
             else:
-                key = key[1:] + l
-                j += 1
+                key += l
 
-print(f'part 1: {i}')
-print(f'part 2: {j}')
+
+print(f'part 2: {len(key)}')
