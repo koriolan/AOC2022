86f8ca29d53690439819ee77a3232cf06f8d4231
Anton <a.bragin@saga-tech.ru>
2022-12-23 16:38:35 +0300

    Краткий вариант 22

--- day22.py
+++ day22.py
@@ -4,212 +4,46 @@ start = pfc()
 with open('input/22.txt') as f:
     maps, path = f.read().split('\n\n')
     maps = maps.split('\n')
-
-path = path.replace('R', ' R ').replace('L', ' L ').split(' ')
-
-
-def findUp(sm, pos):
-    for i in range(len(maps) - 1, -1, -1):
-        if len(maps[i]) > pos[0]:
-            if maps[i][pos[0]] == '.':
-                pos[1] = i
-                sm -= 1
-                break
-            elif maps[i][pos[0]] == '#':
-                sm = 0
-                break
-    return sm
-
-
-def findLeft(sm, pos):
-    for i in range(len(maps[pos[1]]) - 1, -1, -1):
-        if maps[pos[1]][i] == '.':
-            pos[0] = i
-            sm -= 1
-            break
-        elif maps[pos[1]][i] == '#':
-            sm = 0
-            break
-    return sm
-
-
-def findDown(sm, pos):
     for i in range(len(maps)):
-        if maps[i][pos[0]] == '.':
-            pos[1] = i
-            sm -= 1
-            break
-        elif maps[i][pos[0]] == '#':
-            sm = 0
-            break
-    return sm
+        maps[i] = maps[i].ljust(150, ' ')
+path = list(map(int, path.replace('R', ' 1 ').replace('L', ' -1 ').split(' ')+['1']))
 
 
-def findRight(sm, pos):
-    for i in range(len(maps[pos[1]])):
-        if maps[pos[1]][i] == '.':
-            pos[0] = i
-            sm -= 1
-            break
-        elif maps[pos[1]][i] == '#':
-            sm = 0
-            break
-    return sm
-
-
-def part(part1=True):
+def part(part):
+    part -= 1
     position = [maps[0].index('.'), 0]
     direction = 0
+    z = (((lambda x, y: (0, [50, y]), lambda x, y: (0, [50, y]), lambda x, y: (0, [0, y]), lambda x, y: (0, [0, y])),
+          (lambda x, y: (2, [99, 149 - y]), lambda x, y: (3, [50 + y, 49]),
+           lambda x, y: (2, [149, 149 - y]), lambda x, y: (3, [y - 100, 149]))),
+         ((lambda x, y: (1, [x, 100]), lambda x, y: (1, [x, 0]), lambda x, y: (1, [x, 0])),
+          (lambda x, y: (1, [100 + x, 0]), lambda x, y: (2, [49, 100 + x]), lambda x, y: (2, [99, x - 50]))),
+         ((lambda x, y: (2, [149, y]), lambda x, y: (2, [99, y]), lambda x, y: (2, [99, y]), lambda x, y: (2, [49, y])),
+          (lambda x, y: (0, [0, 149 - y]), lambda x, y: (1, [y - 50, 100]), lambda x, y: (0, [50, 149 - y]),
+           lambda x, y: (1, [y - 100, 0]))),
+         ((lambda x, y: (3, [x, 199]), lambda x, y: (3, [x, 149]), lambda x, y: (3, [x, 49])),
+          (lambda x, y: (0, [50, 50 + x]), lambda x, y: (0, [0, 100 + x]), lambda x, y: (3, [x - 100, 199]))))
+    ww = (
+        lambda x, y, d, pa: z[d][pa][y // 50](x, y) if len(maps[y]) <= x + 1 or maps[y][x + 1] == ' ' else (
+            d, [x + 1, y]),
+        lambda x, y, d, pa: z[d][pa][x // 50](x, y) if len(maps) <= y + 1 or maps[y + 1][x] == ' ' else (d, [x, y + 1]),
+        lambda x, y, d, pa: z[d][pa][y // 50](x, y) if x - 1 < 0 or maps[y][x - 1] == ' ' else (d, [x - 1, y]),
+        lambda x, y, d, pa: z[d][pa][x // 50](x, y) if y - 1 < 0 or maps[y - 1][x] == ' ' else (d, [x, y - 1]))
 
     for p in range(0, len(path), 2):
-        sm = int(path[p])
+        sm = path[p]
         while sm > 0:
-            if direction == 0:  # right
-                if len(maps[position[1]]) <= position[0] + 1 or maps[position[1]][position[0] + 1] == ' ':
-                    if part1:
-                        sm = findRight(sm, position)
-                    else:
-                        new_pos = []
-                        new_dir = 0
-                        if 0 <= position[1] < 50:
-                            new_dir = 2
-                            new_pos = [99, 100 + (49 - position[1])]
-                        elif 50 <= position[1] < 100:
-                            new_dir = 3
-                            new_pos = [100 + position[1] - 50, 49]
-                        elif 100 <= position[1] < 150:
-                            new_dir = 2
-                            new_pos = [149, 49-(position[1] - 100)]
-                        elif 150 <= position[1] < 200:
-                            new_dir = 3
-                            new_pos = [50 + position[1] - 150, 149]
-                        else:
-                            assert 1 != 1, f'{position}, Right'
+            new_dir, new_pos = ww[direction](position[0], position[1], direction, part)
+            direction, position, sm = (new_dir, new_pos, sm-1) if maps[new_pos[1]][new_pos[0]] == '.' else (direction, position, 0)
 
-                        if maps[new_pos[1]][new_pos[0]] == '#':
-                            sm = 0
-                        else:
-                            direction = new_dir
-                            position = new_pos
-                            sm -= 1
-                elif maps[position[1]][position[0] + 1] == '.':
-                    position[0] += 1
-                    sm -= 1
-                else:
-                    sm = 0
-
-            elif direction == 1:  # Down
-                if len(maps) <= position[1] + 1 or len(maps[position[1] + 1]) <= position[0] or maps[position[1] + 1][position[0]] == ' ':
-                    if part1:
-                        sm = findDown(sm, position)
-                    else:
-                        new_pos = []
-                        new_dir = 0
-                        if 0 <= position[0] < 50:
-                            new_dir = 1
-                            new_pos = [100+position[0], 0]
-                        elif 50 <= position[0] < 100:
-                            new_dir = 2
-                            new_pos = [49, 150 + position[0] - 50]
-                        elif 100 <= position[0] < 150:
-                            new_dir = 2
-                            new_pos = [99, 50 + position[0] - 100]
-                        else:
-                            assert 1 != 1, f'{position}, Down'
-
-                        if maps[new_pos[1]][new_pos[0]] == '#':
-                            sm = 0
-                        else:
-                            direction = new_dir
-                            position = new_pos
-                            sm -= 1
-
-                elif maps[position[1] + 1][position[0]] == '.':
-                    position[1] += 1
-                    sm -= 1
-                else:
-                    sm = 0
-
-            elif direction == 2:  # LEFT
-                if position[0] - 1 < 0 or maps[position[1]][position[0] - 1] == ' ':
-                    if part1:
-                        sm = findLeft(sm, position)
-                    else:
-                        new_pos = []
-                        new_dir = 0
-                        if 0 <= position[1] < 50:
-                            new_dir = 0
-                            new_pos = [0, 100 + (49 - position[1])]
-                        elif 50 <= position[1] < 100:
-                            new_dir = 1
-                            new_pos = [position[1] - 50, 100]
-                        elif 100 <= position[1] < 150:
-                            new_dir = 0
-                            new_pos = [50, 49 - (position[1] - 100)]
-                        elif 150 <= position[1] < 200:
-                            new_dir = 1
-                            new_pos = [50 + position[1] - 150, 0]
-                        else:
-                            assert 1 != 1, f'{position}, Left'
-                        if maps[new_pos[1]][new_pos[0]] == '#':
-                            sm = 0
-                        else:
-                            direction = new_dir
-                            position = new_pos
-                            sm -= 1
-                elif maps[position[1]][position[0] - 1] == '.':
-                    position[0] -= 1
-                    sm -= 1
-                else:
-                    sm = 0
-
-            elif direction == 3:  # UP
-                if position[1] - 1 < 0 or len(maps[position[1] - 1]) <= position[0] or maps[position[1] - 1][position[0]] == ' ':
-                    if part1:
-                        sm = findUp(sm, position)
-                    else:
-                        new_pos = []
-                        new_dir = 0
-                        if 0 <= position[0] < 50:
-                            new_dir = 0
-                            new_pos = [50, 50 + position[0]]
-                        elif 50 <= position[0] < 100:
-                            new_dir = 0
-                            new_pos = [0, 150 + position[0] - 50]
-                        elif 100 <= position[0] < 150:
-                            new_dir = 3
-                            new_pos = [position[0]-100, 199]
-                        else:
-                            assert 1 != 1, f'{position}, Up'
-                        if maps[new_pos[1]][new_pos[0]] == '.':
-                            direction = new_dir
-                            position = new_pos
-                            sm -= 1
-                        else:
-                            sm = 0
-                elif maps[position[1] - 1][position[0]] == '.':
-                    position[1] -= 1
-                    sm -= 1
-                else:
-                    sm = 0
-            else:
-                assert 1 != 1, f'{direction}'
-
-        if len(path) > p + 1:
-            if path[p + 1] == 'R':
-                direction += 1
-                if direction > 3:
-                    direction = 0
-            else:
-                direction -= 1
-                if direction < 0:
-                    direction = 3
+        direction += path[p + 1]
+        direction = 0 if direction > 3 else 3 if direction < 0 else direction
 
     return (position[1] + 1) * 1000 + (position[0] + 1) * 4 + direction
 
 
-Part1 = part()
-Part2 = part(False)
+Part1 = part(1)
+Part2 = part(2)
 print(f'Part 1:{Part1}')
 print(f'Part 2:{Part2}')
 print(f'time: {pfc() - start}')
