7869aae10eaa8e2e82fd9a47124852db61b410eb
Anton <a.bragin@saga-tech.ru>
2023-09-27 15:32:17 +0300

    ускоренны 2 и 3 задача

--- day2.py
+++ day2.py
@@ -1,7 +1,9 @@
 from time import perf_counter as pfc
 start = pfc()
 
-score = {'A X\n': [4, 3], 'A Y\n': [8, 4], 'A Z\n': [3, 8], 'B X\n': [1, 1], 'B Y\n': [5, 5], 'B Z\n': [9, 9], 'C X\n': [7, 2], 'C Y\n': [2, 6], 'C Z\n': [6, 7]}
-print(f'part 1:{sum(map(lambda x: score[x][0], open("input/2.txt").readlines()))}')
-print(f'part 2:{sum(map(lambda x: score[x][1], open("input/2.txt").readlines()))}')
+score = {b'A X\n': (4, 3), b'A Y\n': (8, 4), b'A Z\n': (3, 8), b'B X\n': (1, 1), b'B Y\n': (5, 5), b'B Z\n': (9, 9), b'C X\n': (7, 2), b'C Y\n': (2, 6), b'C Z\n': (6, 7)}
+with open("input/2.txt",'rb') as f:
+    lines = f.readlines()
+    print(f'part 1:{sum(map(lambda x: score[x][0], lines))}')
+    print(f'part 2:{sum(map(lambda x: score[x][1], lines))}')
 print(f'time: {pfc() - start}')
--- day3.py
+++ day3.py
@@ -1,13 +1,11 @@
 from time import perf_counter as pfc
-import string
 
 start = pfc()
 
-prior = {}
-for idx, i in enumerate(string.ascii_letters):
-    prior[i] = idx+1
-with open('input/3.txt') as f:
+prior = lambda x: (x-38) % 58
+
+with open('input/3.txt', 'rb') as f:
     lines = f.readlines()
-    print(f'part 1: {sum([prior[(set(l[:len(l)//2])&set(l[len(l)//2:])).pop()] for l in lines])}')
-    print(f'part 2: {sum([prior[set.intersection(*[set(l[:-1]) for l in lines[i:i+3]]).pop()] for i in range(0,len(lines),3)])}')
+    print(f'part 1: {sum([prior((set(l[:len(l)//2])&set(l[len(l)//2:])).pop()) for l in lines])}')
+    print(f'part 2: {sum([prior(set.intersection(*[set(l[:-1]) for l in lines[i:i+3]]).pop()) for i in range(0,len(lines),3)])}')
 print(f'time: {pfc() - start}')
